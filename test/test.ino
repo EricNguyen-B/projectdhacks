909b34b21d94acb7966c00e59b132230042c272c
Rafael Perez <rafaelandresph@gmail.com>
2020-02-22 20:21:22 -0500

    Added a while loop for the test

--- test/test.ino
+++ test/test.ino
@@ -1,17 +1,53 @@
+// defines pins numbers
+const int trigPin = 5;
+const int echoPin = 6;
+const int RED = 13;
+const int GREEN = 11;
+
+// defines variables
+long duration;
+int distance;
+int distance = 0;
+const int car = 0;
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+// Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+  distance= (duration / 29) / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+// Set If-Else statement to declare which lights should turn on
+  while (distance <= 500){
+    if (distance > 200) {
+      digitalWrite(RED, HIGH);
+      digitalWrite(GREEN, LOW);
+      
+      }
+    else if (distance < 200){
+      digitalWrite(RED, LOW);
+      digitalWrite(GREEN, HIGH);
+      }
+    else {
+      Serial.print("Cannot detect");
+    }
+    
+  }
 }
